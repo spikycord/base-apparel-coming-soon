80acb36544b32bf05ab17644a196641d21b40ecb
skozmi <skozmi@gmail.com>
2022-10-14 14:20:21 +0200

    finished project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,15 +10,34 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+let message = {
+    error: {text:'', class:''},
+    success: {text: ''}
+};
 
 app.get('/', (req, res, next)=>{
-    res.render('main');
+    res.render('main', {errorMsg: message.error.text, errorIcon: message.error.class, successMsg: message.success.text});
 });
 
-/* app.post('/', (req, res, next)=>{
-    let suma = '$149.99';
-    res.render('main', {selectSum: suma})
-}); */
+app.post('/', (req, res, next)=>{
+    message = {
+        error: {text:'', class:''},
+        success: {text: ''}
+    };
+    let emailInput = req.body.email;
+    if(!emailInput || emailInput.match(/@/) == null || emailInput.match(/.com/) == null){
+        message.error.text = 'Please provide a valid email';
+        message.error.class = 'error-input'
+    }
+    else {
+        message.success.text = 'Thank you for login in';
+    }
+    res.render('main', {errorMsg: message.error.text, errorIcon: message.error.class, successMsg: message.success.text});
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`);
